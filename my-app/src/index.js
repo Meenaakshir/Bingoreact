6ed2058406b11a4c1a09244c61a74c8bb7e79fc7
Meenaakshir <meenaakshivra@gmail.com>
2020-12-19 14:38:38 -0500

    adding styles

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 /*import React from "react";*/
 import ReactDOM from "react-dom";
 import Game from "./components/Game.js";
+import "./index.css";
 
 function App() {
   return (
